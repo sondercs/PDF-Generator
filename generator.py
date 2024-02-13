314d4e335097608fe176021ffa1a2dd81c1cc3f8
João Pedro Alexandrino <sonderinterprise@gmail.com>
2024-02-13 14:21:03 -0300

    Configured the code to the template and create an example.

--- generator.py
+++ generator.py
@@ -1,22 +1,20 @@
 from fpdf import FPDF
 
-project_description = input('Enter the project description: ')
+project_name = input('Enter the project name: ')
 estimated_hours = input('Enter the total estimated hours: ')
-hours_worked = input('Enter the total hours worked: ')
-deadline = input('Enter the estimated deadline: ')
+hours_value = input('Enter the total price of the hour worked: ')
 
-total_value = int(estimated_hours) * int(hours_worked)
+total_value = int(estimated_hours) * int(hours_value)
 
 pdf = FPDF()
 pdf.add_page()
 pdf.set_font("Arial")
-pdf.image("template.png", x=0, y=0)
+pdf.image("template.png", x=0, y=0, w=210, h=290)
 
-pdf.text(115, 145, project_description) # Precisa de correção
-pdf.text(115, 160, estimated_hours) # Precisa de correção
-pdf.text(115, 175, hours_worked) # Precisa de correção
-pdf.text(115, 190, deadline) # Precisa de correção
-pdf.text(115, 205, str(total_value)) # Precisa de correção
+pdf.text(147, 126, project_name) # Nome do projeto
+pdf.text(169, 138, estimated_hours) # Estimativas de horas
+pdf.text(169, 150, hours_value) # valor da hora
+pdf.text(157, 173, "R$" + str(total_value)) # Valor total
 
 pdf.output("Budget.pdf")
 print("Budget generated successfully!")
